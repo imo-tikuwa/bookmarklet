8652a56473e10fe956ad27ef0c60464b25f4a335
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-04-01 21:49:08 +0900

    コピー対象を橙の枠で囲むようmouseoverのイベント処理を追記してみる
    ブックマークレットを生成するのに使用するツールを以下に変更（ソースにコメント入れてもよくなったみたい）
    前：https://ytyng.github.io/bookmarklet-script-compress/
    後：http://ted.mielczarek.org/code/mozilla/bookmarklet.html

--- src/table-row-to-clipbord.js
+++ src/table-row-to-clipbord.js
@@ -2,7 +2,19 @@
   if (!navigator.clipboard) {
     alert("お使いのブラウザはこのブックマークレットに対応していません。。；；");
   }
+
+  // CSS作成
+  var style = document.createElement("style");
+  document.head.appendChild(style);
+  var sheet = style.sheet;
+  sheet.insertRule("tr:first-child .table-row-to-clipbord-current { border-top: 2px solid rgba(242, 120, 75, 1) !important; }", 0);
+  sheet.insertRule(".table-row-to-clipbord-current { background-color:rgba(242, 120, 75, 0.15) !important;border-right: 2px solid rgba(242, 120, 75, 1) !important;border-left: 2px solid rgba(242, 120, 75, 1) !important; }", 1);
+  sheet.insertRule("tbody tr:last-child .table-row-to-clipbord-current { border-bottom: 2px solid rgba(242, 120, 75, 1) !important; }", 2);
+  sheet.insertRule("thead ~ tbody tr:first-child .table-row-to-clipbord-current { border-top: inherit !important; }", 3);
+
   alert("テーブルのコピーしたい列をクリックしてください");
+
+  // テーブルのセルをクリックしたときのコピー処理
   document.body.onclick = function(event) {
     let $target = $(event.target);
     if ($.inArray($target.prop('tagName'), ['TH', 'TD']) == -1) {
@@ -21,5 +33,25 @@
     navigator.clipboard.writeText(clip_data.join('\r\n'));
     alert("クリップボードにコピーしました");
     document.body.onclick = null;
-  }
+    document.body.onmouseover = null;
+    $(document).find('.table-row-to-clipbord-current').removeClass('table-row-to-clipbord-current');
+  };
+
+  // テーブルをマウスオーバーしたときのハイライト表示処理
+  document.body.onmouseover = function(event) {
+    let $target = $(event.target);
+    if ($.inArray($target.prop('tagName'), ['TH', 'TD']) == -1) {
+      return;
+    }
+    $table = $target.parents('table');
+    let clicked_index = $target.index();
+    $table.find('tr').each(function(tr_index, tr_element){
+      $(tr_element).find('th,td').each(function(col_index, col_element){
+        $(col_element).removeClass('table-row-to-clipbord-current');
+        if (col_index == clicked_index) {
+          $(col_element).addClass('table-row-to-clipbord-current');
+        }
+      });
+    });
+  };
 });
