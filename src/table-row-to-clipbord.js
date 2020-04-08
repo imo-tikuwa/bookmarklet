e9634a5166c49784bbf6520f1ef2badcda88fe30
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-04-08 20:48:36 +0900

    mouseleaveの処理追加、mouseoverの処理修正など

--- src/table-row-to-clipbord.js
+++ src/table-row-to-clipbord.js
@@ -4,18 +4,18 @@
   var style = document.createElement("style");
   document.head.appendChild(style);
   var sheet = style.sheet;
-  sheet.insertRule("tr:first-child .table-row-to-clipbord-current { border-top: 2px solid rgba(242, 120, 75, 1) !important; }", 0);
+  sheet.insertRule(".table-row-to-clipbord-current.table-row-to-clipbord-current-head { border-top: 2px solid rgba(242, 120, 75, 1) !important; }", 0);
   sheet.insertRule(".table-row-to-clipbord-current { background-color:rgba(242, 120, 75, 0.15) !important;border-right: 2px solid rgba(242, 120, 75, 1) !important;border-left: 2px solid rgba(242, 120, 75, 1) !important; }", 1);
-  sheet.insertRule("tbody tr:last-child .table-row-to-clipbord-current { border-bottom: 2px solid rgba(242, 120, 75, 1) !important; }", 2);
-  sheet.insertRule("thead ~ tbody tr:first-child .table-row-to-clipbord-current { border-top: inherit !important; }", 3);
+  sheet.insertRule(".table-row-to-clipbord-current.table-row-to-clipbord-current-bottom { border-bottom: 2px solid rgba(242, 120, 75, 1) !important; }", 2);
 
   alert("テーブルのコピーしたい列をクリックしてください");
 
   // テーブルのセルをクリックしたときのコピー処理
   document.body.onclick = function(event) {
+    event.preventDefault();
     let $target = $(event.target);
     if ($.inArray($target.prop('tagName'), ['TH', 'TD']) == -1) {
-      return;
+      return false;
     }
     $table = $target.parents('table');
     let clicked_index = $target.index();
@@ -38,7 +38,7 @@
 
     document.body.onclick = null;
     document.body.onmouseover = null;
-    $(document).find('.table-row-to-clipbord-current').removeClass('table-row-to-clipbord-current');
+    $(document).find('.table-row-to-clipbord-current').removeClass('table-row-to-clipbord-current table-row-to-clipbord-current-head table-row-to-clipbord-current-bottom');
   };
 
   // テーブルをマウスオーバーしたときのハイライト表示処理
@@ -49,13 +49,25 @@
     }
     $table = $target.parents('table');
     let clicked_index = $target.index();
+    let line_count = $table.find('tr').length;
     $table.find('tr').each(function(tr_index, tr_element){
       $(tr_element).find('th,td').each(function(col_index, col_element){
-        $(col_element).removeClass('table-row-to-clipbord-current');
+        $(col_element).removeClass('table-row-to-clipbord-current table-row-to-clipbord-current-head table-row-to-clipbord-current-bottom');
         if (col_index == clicked_index) {
-          $(col_element).addClass('table-row-to-clipbord-current');
+          if (tr_index == 0) {
+            $(col_element).addClass('table-row-to-clipbord-current table-row-to-clipbord-current-head');
+          } else if (tr_index == line_count - 1) {
+            $(col_element).addClass('table-row-to-clipbord-current table-row-to-clipbord-current-bottom');
+          } else {
+            $(col_element).addClass('table-row-to-clipbord-current');
+          }
         }
       });
     });
   };
+
+  // カーソルがテーブルを抜けたときの処理
+  $(document).find("table").on("mouseleave", function(e){
+    $(event.target).parents('table').find('.table-row-to-clipbord-current').removeClass('table-row-to-clipbord-current');
+  });
 });
