2909b6bb13f7bcf18d4fbba1eb72aaa54e00ecaf
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-06-11 21:16:06 +0900

    Amazon.co.jpから出品されている商品のみを表示するブックマークレットを作成

---
+++ src/filter-seller-only-amazon.js
@@ -0,0 +1,16 @@
+if (location.origin != 'https://www.amazon.co.jp' || location.pathname != '/s') {
+  alert("このブックマークレットはAmazon.co.jpの検索結果一覧で実行してください");
+  return;
+}
+
+if (window.confirm('Amazon.co.jpから出品されている商品のみを表示します')) {
+  let query = location.search,
+  regex = /rh=(.*?)(&|$)/,
+  filter = 'rh=p_6:AN1VRQENFRJN5';
+  if (query.match(regex)) {
+    query = query.replace(regex, filter + '&');
+  } else {
+    query += '&' + filter;
+  }
+  location.href = location.origin + location.pathname + query;
+}
